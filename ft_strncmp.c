8111f6f9577a246e00a3c4d3b9a8520bbeb67f5c
Thanh-cong VO <tvo@e2r5p1.clusters.42paris.fr>
2022-11-19 22:03:05 +0100

    update second part

---
+++ ft_strncmp.c
@@ -0,0 +1,37 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strncmp.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tvo <tvo@student.42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/11/19 19:21:57 by tvo               #+#    #+#             */
+/*   Updated: 2022/11/19 21:48:07 by tvo              ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <string.h>
+#include <stdio.h>
+
+int ft_strncmp(char *s1, char *s2, size_t n)
+{
+    size_t i;
+    
+    i = 0;
+    while (s1[i] && s2[i] && (i < n))
+    {
+        if (s1[i] == s2[i])
+            i++;
+        else
+            break ;
+    }
+    return (s1[i] - s2[i]);
+}
+
+int main()
+{
+    char str1[] = "";
+    char str2[] = "";
+    printf("%d \n", ft_strncmp(str1, str2, 5));
+    printf("result is %d", strncmp(str1, str2, 5));
+}
--- ft_strrchr.c
+++ ft_strrchr.c
@@ -1,58 +1,39 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_strchr.c                                        :+:      :+:    :+:   */
+/*   ft_strrchr.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: tvo <tvo@student.42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/11/18 22:27:44 by tvo               #+#    #+#             */
-/*   Updated: 2022/11/19 19:19:52 by tvo              ###   ########.fr       */
+/*   Created: 2022/11/19 17:13:03 by tvo               #+#    #+#             */
+/*   Updated: 2022/11/19 19:20:37 by tvo              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <string.h>
 #include <stdio.h>
 
-char *ft_strchr(char *str, int c)
+char *ft_strrchr(char *str, int c)
 {
     int i;
-    
-    // ptr = NULL;
+
     i = 0;
-    while (str[i] != 0)
+    while (str[i])
+        i++;
+    while (i > 0)
     {
         if (str[i] == c)
             return (&str[i]);
-        i++;
+        i--;
     }
     return (NULL);
 }
 
-// char *ft_strchr(char *str, int c)
-// {
-//     int i;
-//     int size;
-
-//     size = strlen(str);
-//     if (!str)
-//         return (NULL);
-//     i = 0;
-//     while(i < size)
-//     {
-//         if (str[i] == c)
-//             return ((char*)str + i);
-//         i++;
-//     }
-// }
-
 int main()
 {
-    char str1[] = "stotal42";
-    char *p;
-    char c;
-
-    c = 't';
-    p = ft_strchr(str1, c);
-    // printf("result 1 is: %s \n", strchr(str1, c));
-    printf("result 2 is: %s", p);   
+    char *ch = "stotal42";
+    char c = 't';
+    char *p = strrchr(ch, c);
+    
+    printf("result is %s", p);
 }
