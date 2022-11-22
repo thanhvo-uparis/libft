e39d2f50104658fec945c70323569662fe200875
Thanh Cong Vo <thanhvo.uparis@gmail.com>
2022-11-22 23:17:03 +0100

    add memchr, memcmp

--- ft_memchr.c
+++ ft_memchr.c
@@ -6,11 +6,40 @@
 /*   By: tvo <tvo@student.42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/19 19:22:26 by tvo               #+#    #+#             */
-/*   Updated: 2022/11/19 22:00:59 by tvo              ###   ########.fr       */
+/*   Updated: 2022/11/20 21:55:29 by tvo              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdio.h>
 #include <string.h>
 
-void *memchr(void *s, int c, size_t n)
+void *ft_memchr(void *s, int c, size_t n)
+{
+    size_t i;
+
+    i = 0;
+    if (!s)
+        return (NULL);
+    while (i < n)
+    {
+        if (*((unsigned char*)(s + i)) == c)
+            return ((void*)(s + i));
+        i++;
+    }
+    return (NULL);
+}
+
+int main()
+{
+    char str1[] = "vncoding forum";
+    int c;
+    char *ptr;
+
+    c = 'o';
+    ptr = (char*)ft_memchr(str1, c, 3);
+    // ptr = (char*)memchr(str1, c, 3);
+    if (ptr != NULL)
+        printf("%c is at position %ld", c, ptr-str1+1);
+    else
+        printf("not found!");
+}
--- ft_memcmp.c
+++ ft_memcmp.c
@@ -6,7 +6,42 @@
 /*   By: tvo <tvo@student.42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/19 19:22:47 by tvo               #+#    #+#             */
-/*   Updated: 2022/11/19 19:22:48 by tvo              ###   ########.fr       */
+/*   Updated: 2022/11/20 23:24:34 by tvo              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <stdio.h>
+#include <string.h>
+
+int ft_memcmp(void *s1, void *s2, size_t n)
+{
+    size_t i;
+
+    i = 0;
+    if (!((char*)s1))
+        return (0);
+    while (i < n)
+    {
+        if (*((unsigned char*)(s1 + i)) == *((unsigned char*)(s2 + i)))
+            i++;
+        else
+            return (*((unsigned char*)(s1 + i)) - *((unsigned char*)(s2 + i)));
+
+    }
+    return (0);
+}
+
+int main()
+{
+    char *str1 = "studENT";
+    char *str2 = "student";
+    int res;
+    
+    res = ft_memcmp(str1, str2, 7);
+    if (res > 0)
+        printf("str1 est supérieur à str2");
+    else if (res < 0)
+        printf("str1 est inférieur à str2");
+    else
+        printf("str1 est également à str2");
+}
